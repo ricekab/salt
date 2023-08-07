ac84b373cdf3d444f7796e4174aa5d16037bd6b3
ricekab <contact@kevinchiyantang.com>
2023-08-07 14:41:22 +0200

    todo

--- tests/pytests/unit/roster/test_terraform.py
+++ tests/pytests/unit/roster/test_terraform.py
@@ -21,6 +21,12 @@ def roster_file_new():
 
 
 @pytest.fixture
+def roster_file_priv():
+    """ Fixture containing a specified "priv" argument. """
+    return pathlib.Path(__file__).parent / "terraform.data" / "terraform-new-specific-priv.tfstate"
+
+
+@pytest.fixture
 def pki_dir():
     return pathlib.Path(__file__).parent / "terraform.data"
 
@@ -165,7 +171,7 @@ def test_specified_priv(pki_dir, roster_file_new):
             "user": "root",
             "passwd": "linux",
             "timeout": 22,
-            "priv": str(pki_dir / "ssh" / "salt-ssh-specified.rsa"),
+            "priv": str(pki_dir / "ssh" / "salt-ssh.rsa"),
         },
         "web1": {
             "host": "192.168.122.107",
@@ -176,10 +182,7 @@ def test_specified_priv(pki_dir, roster_file_new):
         },
     }
 
-    with patch.dict(terraform.__opts__, {
-            "roster_file": str(roster_file_new),
-            "priv": str(pki_dir / "ssh" / "salt-ssh-specified.rsa")
-            }):
+    with patch.dict(terraform.__opts__, {"roster_file": str(roster_file_priv)}):
         ret = terraform.targets("*")
         assert expected_result == ret
 
